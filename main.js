9c207f949bd9bfc84f6e8d5355a180101718dc68
SecondOpportunity3D <josalusa13@gmail.com>
2024-07-30 12:56:44 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function muestra_oculta(id) {
-    let div = document.getElementById(id);
-    if (div.style.display == ""){
-        div.style.display = "none";
-    }
-    else{
-        div.style.display = "";
-    }
-} 
